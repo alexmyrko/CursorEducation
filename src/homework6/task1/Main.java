1bac96605569aefbf3e445e5cb072f24f93b03b7
alexmyrko <alex.myrko@gmail.com>
2021-01-13 10:40:41 +0200

    Refactor folders structure

--- src/homework6/task1/Main.java
+++ src/homework6/task1/Main.java
@@ -1,10 +1,5 @@
 package homework6.task1;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("ArrayList Tasks");
